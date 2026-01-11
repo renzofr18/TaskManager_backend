acdc609e9ea0079be70b5d33d39840bf9781dcbe
renzo <renzofalconi22@hotmail.com>
2026-01-11 16:09:38 -0500

    feat(domain): add core domain entities and enums

---
+++ TaskManager.Domain/Entities/User.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+using TaskManager.Domain.Common;
+
+namespace TaskManager.Domain.Entities
+{
+    public class User : BaseEntity
+    {
+        public string Username { get; private set; } = null;
+        public string Email { get; private set; } = null;
+
+        protected User() { }
+
+        public User(string username, string email)
+        {
+            Id = Guid.NewGuid();
+            Username = username;
+            Email = email;
+            CreatedAt = DateTime.Now;   
+        }
+    }
+}
