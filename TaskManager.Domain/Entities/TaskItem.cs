bbe0a3c4004fc39b15f422bfe674ccd32b7aecbf
renzo <renzofalconi22@hotmail.com>
2026-01-11 16:11:30 -0500

    fix compilation changes

--- TaskManager.Domain/Entities/TaskItem.cs
+++ TaskManager.Domain/Entities/TaskItem.cs
@@ -1,12 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using TaskManager.Domain.Common;
+using TaskManager.Domain.Enums;
 
 namespace TaskManager.Domain.Entities
 {
-    internal class TaskItem
+    public class TaskItem: BaseEntity
     {
+        public string Tittle { get; private set; } = null;
+        public string Description { get; private set; } = null;
+        public TaskStatus Status { get; private set; } 
+        public TaskPriority Priority { get; private set; } 
+        public Guid UserId { get; private set; } 
+
+        protected TaskItem() { }
+
+        public TaskItem(string tittle, string description, TaskStatus status, TaskPriority priority, Guid userId)
+        {
+            Id = Guid.NewGuid();
+            Tittle = tittle;
+            Description = description;
+            Status = status;
+            Priority = priority;
+            UserId = userId;
+            CreatedAt = DateTime.UtcNow;
+        }
+
+        public void MarkAsCompleted()
+        {
+            Status = TaskStatus.Completed;
+            UpdatedAt = DateTime.UtcNow;
+        }
     }
 }
